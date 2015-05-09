679810895838ea619ed237693c1d693e5c71b8af
Apirak Panatkool <apirakb@gmail.com>
2015-05-09 08:14:09 +0700

    Add handlebars for generate table

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,3 +11,30 @@ $(function () {
     	format: 'DD/MM/YYYY'
     });
 });
+
+var data = { users: [
+  {username: "alan", firstName: "Alan", lastName: "Johnson", email: "alan@test.com" },
+  {username: "allison", firstName: "Allison", lastName: "House", email: "allison@test.com" },
+  {username: "ryan", firstName: "Ryan", lastName: "Carson", email: "ryan@test.com" }
+]};
+
+var data2 = { users: [
+  {username: "bank", firstName: "Alan", lastName: "Johnson", email: "alan@test.com" },
+  {username: "allison", firstName: "Allison", lastName: "House", email: "allison@test.com" },
+  {username: "ryan", firstName: "Ryan", lastName: "Carson", email: "ryan@test.com" }
+]};
+
+$(function () {
+	// handlebars
+	var source   = $("#some-template").html();
+	var template = Handlebars.compile(source);
+
+	$("#content-placeholder").html(template(data));
+
+	// data.users[0].username = "bank"
+
+	data = data2;
+
+	$("#content-placeholder").html(template(data));
+
+});
