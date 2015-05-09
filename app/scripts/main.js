838bab098b46e35ec5723aaf62edbb712de62450
Apirak Panatkool <apirakb@gmail.com>
2015-05-09 15:38:09 +0700

    manage java script file

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,119 +1,39 @@
 $('#myTab a').click(function (e) {
-  e.preventDefault()
-  $(this).tab('show')
-})
-
-$(function () {
-    $('#datetimepickerDeparture').datetimepicker({
-    	format: 'DD/MM/YYYY'
-    });
-    $('#datetimepickerReturn').datetimepicker({
-    	format: 'DD/MM/YYYY'
-    });
-    $("#scrollPrice").slider({
-      id: "price-search-slider",
-      min: 0,
-      max: 200,
-      range: true,
-      value: [0,200]
-    });
-    $("#scrollPrice").on("slide", function(slideEvt) {
-      $("#priceMin").text(slideEvt.value[0]);
-      $("#priceMax").text(slideEvt.value[1]);
-    });
+  'use strict';
+  e.preventDefault();
+  $(this).tab('show');
 });
 
-var data = { orders: [
-  {id:"1", airline: "atl", price: 125, flights:{
-  	go:{
-  		id:"AI-202",
-  		from: "PNQ",
-  		to: "DEL",
-  		depart: "10:00 AM",
-  		arive: "12:00 PM"
-  	},
-  	back:{
-  		id:"AI-202",
-  		from: "PNQ",
-  		to: "DEL",
-  		depart: "10:00 AM",
-  		arive: "12:00 PM"
-  	}
-  }},
-  {id:"2", airline: "sia", price: 125, flights:{
-  	go:{
-  		id:"AI-202",
-  		from: "PNQ",
-  		to: "DEL",
-  		depart: "10:00 AM",
-  		arive: "12:00 PM"
-  	},
-  	back:{
-  		id:"AI-202",
-  		from: "PNQ",
-  		to: "DEL",
-  		depart: "10:00 AM",
-  		arive: "12:00 PM"
-  	}
-  }},
-  {id:"3", airline: "swa", price: 125, flights:{
-  	go:{
-  		id:"AI-202",
-  		from: "PNQ",
-  		to: "DEL",
-  		depart: "10:00 AM",
-  		arive: "12:00 PM"
-  	},
-  	back:{
-  		id:"AI-202",
-  		from: "PNQ",
-  		to: "DEL",
-  		depart: "10:00 AM",
-  		arive: "12:00 PM"
-  	}
-  }},
-  {id:"4", airline: "tap", price: 125, flights:{
-    go:{
-      id:"AI-202",
-      from: "PNQ",
-      to: "DEL",
-      depart: "10:00 AM",
-      arive: "12:00 PM"
-    },
-    back:{
-      id:"AI-202",
-      from: "PNQ",
-      to: "DEL",
-      depart: "10:00 AM",
-      arive: "12:00 PM"
-    }
-  }},
-  {id:"5", airline: "vta", price: 125, flights:{
-    go:{
-      id:"AI-202",
-      from: "PNQ",
-      to: "DEL",
-      depart: "10:00 AM",
-      arive: "12:00 PM"
-    },
-    back:{
-      id:"AI-202",
-      from: "PNQ",
-      to: "DEL",
-      depart: "10:00 AM",
-      arive: "12:00 PM"
-    }
-  }}
-]};
+$(function () {
+  'use strict';
+  $('#datetimepickerDeparture').datetimepicker({
+  	format: 'DD/MM/YYYY'
+  });
+  $('#datetimepickerReturn').datetimepicker({
+  	format: 'DD/MM/YYYY'
+  });
+  $('#scrollPrice').slider({
+    id: 'price-search-slider',
+    min: 0,
+    max: 200,
+    range: true,
+    value: [0,200]
+  });
+  $('#scrollPrice').on('slide', function(slideEvt) {
+    $('#priceMin').text(slideEvt.value[0]);
+    $('#priceMax').text(slideEvt.value[1]);
+  });
+});
 
 $(function () {
-	var source   = $("#orders-template").html();
+  'use strict';
+	var source   = $('#orders-template').html();
 	var template = Handlebars.compile(source);
 
-	$("#content-placeholder").html(template(data));
+	$('#content-placeholder').html(template(data));
 
-	// data.users[0].username = "bank"
+  // example for search function
+	// data.users[0].username = 'bank'
 	// data = data2;
-	// $("#content-placeholder").html(template(data));
-
+	// $('#content-placeholder').html(template(data));
 });
