0d5ffbe02189b32ac56bb194da1af9e5b6aeccde
Apirak Panatkool <apirakb@gmail.com>
2015-05-09 16:29:20 +0700

    add dist

---
+++ Readme.md
@@ -0,0 +1,24 @@
+# Readme
+
+
+## Install
+
+Install nodejs first
+
+* $ npm install
+* $ bower install
+
+## Start server
+
+* $ grunt serve
+
+## Generate production
+
+* $ grunt --force
+
+production will save to dist folder
+
+
+## Quick view
+
+open file /dist/index.html on your browser
---
+++ app/scripts/data.js
@@ -0,0 +1,82 @@
+var data = { orders: [
+  {id:'1', airline: 'atl', price: 125, flights:{
+  	go:{
+  		id:'AI-202',
+  		from: 'PNQ',
+  		to: 'DEL',
+  		depart: '10:00 AM',
+  		arive: '12:00 PM'
+  	},
+  	back:{
+  		id:'AI-202',
+  		from: 'PNQ',
+  		to: 'DEL',
+  		depart: '10:00 AM',
+  		arive: '12:00 PM'
+  	}
+  }},
+  {id:'2', airline: 'sia', price: 125, flights:{
+  	go:{
+  		id:'AI-202',
+  		from: 'PNQ',
+  		to: 'DEL',
+  		depart: '10:00 AM',
+  		arive: '12:00 PM'
+  	},
+  	back:{
+  		id:'AI-202',
+  		from: 'PNQ',
+  		to: 'DEL',
+  		depart: '10:00 AM',
+  		arive: '12:00 PM'
+  	}
+  }},
+  {id:'3', airline: 'swa', price: 125, flights:{
+  	go:{
+  		id:'AI-202',
+  		from: 'PNQ',
+  		to: 'DEL',
+  		depart: '10:00 AM',
+  		arive: '12:00 PM'
+  	},
+  	back:{
+  		id:'AI-202',
+  		from: 'PNQ',
+  		to: 'DEL',
+  		depart: '10:00 AM',
+  		arive: '12:00 PM'
+  	}
+  }},
+  {id:'4', airline: 'tap', price: 125, flights:{
+    go:{
+      id:'AI-202',
+      from: 'PNQ',
+      to: 'DEL',
+      depart: '10:00 AM',
+      arive: '12:00 PM'
+    },
+    back:{
+      id:'AI-202',
+      from: 'PNQ',
+      to: 'DEL',
+      depart: '10:00 AM',
+      arive: '12:00 PM'
+    }
+  }},
+  {id:'5', airline: 'vta', price: 125, flights:{
+    go:{
+      id:'AI-202',
+      from: 'PNQ',
+      to: 'DEL',
+      depart: '10:00 AM',
+      arive: '12:00 PM'
+    },
+    back:{
+      id:'AI-202',
+      from: 'PNQ',
+      to: 'DEL',
+      depart: '10:00 AM',
+      arive: '12:00 PM'
+    }
+  }}
+]};
